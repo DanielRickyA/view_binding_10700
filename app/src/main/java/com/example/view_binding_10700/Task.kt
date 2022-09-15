e44ad02e1ce34ba86df740327274f431fa8f9a24
DanielRickyA <danielrickya1404@gmail.com>
2022-09-15 07:42:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10700/Task.kt
@@ -0,0 +1,4 @@
+package com.example.view_binding_10700
+
+data class Task (val chapter:String, val title:String, val details:String) {
+}
---
+++ app/src/main/java/com/example/view_binding_10700/TaskList.kt
@@ -0,0 +1,12 @@
+package com.example.view_binding_10700
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bagun Tidur", "Kebiasaan sehari-hari untuk membuat hari makin indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencari primo buat Gacha Mama Yaemiko tercinta /  Kengi Wangy"),
+        Task("Chapter 3", "Ngopi", "Habis capek Nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "dimana mama Ei?", "Buat Adu Mekanik Showcase Ayaka Pyro"),
+        Task("Chapter 5", "Upload ke Tiktok", "Buat Pamer ke view Showcase Ayaka c6 + Bennett C6")
+
+    )
+}
